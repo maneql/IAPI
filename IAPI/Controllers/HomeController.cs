0bdb1a75a39a1ce245d77708bfbb9b62f4059dba
lpnam <lenam9412@gmail.com>
2020-09-17 17:13:59 +0700

    first commit

---
+++ IAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("API Is Started");
+        }
+    }
+}
